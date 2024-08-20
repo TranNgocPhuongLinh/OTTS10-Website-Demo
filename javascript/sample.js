5fc60492596c30c6a25933d636ebc6562c92d05e
Tran Ngoc Phuong Linh <tranngocphuonglinh308@gmail.com>
2024-08-20 10:42:30 +0700

    Update js

--- javascript/sample.js
+++ javascript/sample.js
@@ -2,28 +2,28 @@ $(function(){
     $("#content").addClass("hide-content");
     $("#loading").show();
 
-    $('#header').load('/header-footer/header.html', function() {
+    $('#header').load('./header-footer/header.html', function() {
         // callback function
     });
-    $('#sidebar-container').load('sidebar.html', function() {
+    // $('#sidebar-container').load('sidebar.html', function() {
+        
+    // });
+    $('#footer').load("./header-footer/footer.html", function() {
         // callback function
     });
-    $('#footer').load("/header-footer/footer.html", function() {
+    $('#contacts').load('./html/contact.html', function() {
         // callback function
     });
-    $('#contacts').load('/html/contact.html', function() {
-        // callback function
-    });
-    $('#loading').load('/html/loading.html', function() {
+    $('#loading').load('./html/loading.html', function() {
         // callback function
     });
 
     $.when(
-        $('#header').load('/header-footer/header.html'),
-        $('#sidebar-container').load('sidebar.html'),
-        $('#footer').load("/header-footer/footer.html"),
-        $('#contacts').load('/html/contact.html'),
-        $('#loading').load('/html/loading.html')
+        $('#header').load('./header-footer/header.html'),
+        // $('#sidebar-container').load('sidebar.html'),
+        $('#footer').load("./header-footer/footer.html"),
+        $('#contacts').load('./html/contact.html'),
+        $('#loading').load('./html/loading.html')
     ).done(function() {
         setTimeout(function() {
             $("#loading").hide();
